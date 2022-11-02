a68ea8c6aeef0a8ba349fc16fd3384d051c7773a
PanevskiSt <steffpanevski@gmail.com>
2022-11-02 23:11:44 +0200

    Add an Extensions folder and configure initial CORS policy

---
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        // configuring CORS to allow sending requests from different domain to our applicaton
+
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()            //should later be changed to be more specific with security in mind
+                .AllowAnyMethod()                   //can specify concrete methods such as "POST", "GET" e.t.c.
+                .AllowAnyHeader());                 //can be specific as for exmaple with "accept", "content-type" e.t.c
+            });
+    }
+}
