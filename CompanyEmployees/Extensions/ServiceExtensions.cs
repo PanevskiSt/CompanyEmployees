2c7f98b70223bf71b89d3846b49f0c81c09f5cb9
PanevskiSt <steffpanevski@gmail.com>
2022-11-02 23:35:08 +0200

    Add IIS configuration and add support for CORS and IIS integraion in the Program file

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -2,7 +2,7 @@
 {
     public static class ServiceExtensions
     {
-        // configuring CORS to allow sending requests from different domain to our applicaton
+        //configuring CORS to allow sending requests from different domain to our applicaton
 
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
@@ -11,6 +11,14 @@
                 builder.AllowAnyOrigin()            //should later be changed to be more specific with security in mind
                 .AllowAnyMethod()                   //can specify concrete methods such as "POST", "GET" e.t.c.
                 .AllowAnyHeader());                 //can be specific as for exmaple with "accept", "content-type" e.t.c
+            });
+
+        //configure IIS integration
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+                        //default options suffice for the time being therefore no property is being initialised
             });
     }
 }
